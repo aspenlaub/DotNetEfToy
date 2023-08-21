e0aa13f81f566e0e495a39b9fc2b13050df230d4
VerBot <verbot@aspenlaub.net>
2023-08-21 20:11:38 +0200

    Package/-s updated to most recent version

---
+++ src/Migrations/20230821181106_MicrosoftEntityFrameworkCoreTools7010.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aspenlaub.Net.GitHub.CSharp.DotNetEfToy.Migrations
+{
+    /// <inheritdoc />
+    public partial class MicrosoftEntityFrameworkCoreTools7010 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
